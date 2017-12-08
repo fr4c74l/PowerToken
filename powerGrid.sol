dbf564c4cf0061baf864cfacb798baf5dc27e5a3
Rodrigo Q. Saramago <deepmarolaest@gmail.com>
2017-12-08 11:14:53 -0200

    Adjust identation

--- powerGrid.sol
+++ powerGrid.sol
@@ -3,43 +3,42 @@ pragma solidity ^0.4.15;
 import './powerToken.sol';
 
 contract PowerGrid {
- address public owner;
- mapping (uint64 => address) tokens;
+  address public owner;
+  mapping (uint64 => address) tokens;
 
- function PowerGrid() {
-   owner = msg.sender;
- }
-
- function hasExpired(uint64 date) returns(bool) {
-   return block.timestamp > date;
- }
-
- modifier verifyExpirationDate(uint64 date) {
-   //TODO: convert uint timestamp to uint64
-   assert(!hasExpired(date));
-   _;
- }
-
- function create(uint64 amount, address destination, uint64 expirationDate) public
-   verifyExpirationDate(expirationDate) 
- {
-   require(msg.sender == owner);
-   address tokenAddr = tokens[expirationDate];
-   PowerToken token;
-   if (tokenAddr == 0) {
-    token = new PowerToken(expirationDate);
-    tokens[expirationDate] = address(token);
-   } else {
-     token = PowerToken(tokenAddr);
-   }
-   token.create(amount, destination);
- }
-
- function destroyToken(uint64 date) public {
-  if (hasExpired(date)) {
-    PowerToken(tokens[date]).destroy();
-    tokens[date] = 0;
+  function PowerGrid() {
+    owner = msg.sender;
   }
- }
 
+  function hasExpired(uint64 date) returns(bool) {
+    return block.timestamp > date;
+  }
+
+  modifier verifyExpirationDate(uint64 date) {
+    //TODO: convert uint timestamp to uint64
+    assert(!hasExpired(date));
+    _;
+  }
+
+  function create(uint64 amount, address destination, uint64 expirationDate) public
+    verifyExpirationDate(expirationDate)
+  {
+    require(msg.sender == owner);
+    address tokenAddr = tokens[expirationDate];
+    PowerToken token;
+    if (tokenAddr == 0) {
+      token = new PowerToken(expirationDate);
+      tokens[expirationDate] = address(token);
+    } else {
+      token = PowerToken(tokenAddr);
+    }
+    token.create(amount, destination);
+  }
+
+  function destroyToken(uint64 date) public {
+    if (hasExpired(date)) {
+      PowerToken(tokens[date]).destroy();
+      tokens[date] = 0;
+    }
+  }
 }
--- powerToken.sol
+++ powerToken.sol
@@ -3,48 +3,48 @@ pragma solidity ^0.4.15;
 import './powerGrid.sol';
 
 contract PowerToken {
- event Consumed(uint64 amount, uint code);
+  event Consumed(uint64 amount, uint code);
 
- address owner;
- uint64 expirationDate;
- mapping(address => uint64) balances;
+  address owner;
+  uint64 expirationDate;
+  mapping(address => uint64) balances;
 
- function PowerToken(uint64 date) {
-   owner = msg.sender;
-   expirationDate = date;
- }
+  function PowerToken(uint64 date) {
+    owner = msg.sender;
+    expirationDate = date;
+  }
 
- modifier verifyExpirationDate(uint64 date) {
-   //TODO: convert uint timestamp to uint64
-   assert(block.timestamp < date);
-   _;
- }
+  modifier verifyExpirationDate(uint64 date) {
+    //TODO: convert uint timestamp to uint64
+    assert(block.timestamp < date);
+    _;
+  }
 
- function create(uint64 amount, address destination) public {
-   require(msg.sender == owner);
-   balances[destination] += amount;
- }
+  function create(uint64 amount, address destination) public {
+    require(msg.sender == owner);
+    balances[destination] += amount;
+  }
 
- function transfer(address to, uint64 amount) public 
-  verifyExpirationDate(expirationDate) 
+  function transfer(address to, uint64 amount) public
+    verifyExpirationDate(expirationDate)
   {
-   require(balances[msg.sender] >= amount);
-   require(balances[to] + amount > balances[to]);
-   balances[msg.sender] -= amount;
-   balances[to] += amount;
- }
+    require(balances[msg.sender] >= amount);
+    require(balances[to] + amount > balances[to]);
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
+  }
 
- function consume(uint64 amount, uint code) public 
-  verifyExpirationDate(expirationDate)
- {
-  require(balances[msg.sender] >= amount);
-  balances[msg.sender] -= amount;
-  Consumed(amount, code);
- }
+  function consume(uint64 amount, uint code) public
+    verifyExpirationDate(expirationDate)
+  {
+    require(balances[msg.sender] >= amount);
+    balances[msg.sender] -= amount;
+    Consumed(amount, code);
+  }
 
- function destroy() {
-   require(msg.sender == owner);
-   address addr = PowerGrid(owner).owner();
-   selfdestruct(addr);
- }
+  function destroy() {
+    require(msg.sender == owner);
+    address addr = PowerGrid(owner).owner();
+    selfdestruct(addr);
+  }
 }
