473e0fbaadd6e0a7f109c4777818c42ff29b18f9
Lucas Clemente Vella <lvella@gmail.com>
2017-12-10 18:34:15 -0200

    Using timestamps for time verification.
    Now using tabs for indentation.

--- powerGrid.sol
+++ powerGrid.sol
@@ -3,42 +3,38 @@ pragma solidity ^0.4.15;
 import './powerToken.sol';
 
 contract PowerGrid {
-  address public owner;
-  mapping (uint64 => address) tokens;
+	address public owner;
 
-  function PowerGrid() {
-    owner = msg.sender;
-  }
+	// Maps exparation date (in months since Epoch) to the address
+	// of the PowerToken contract. PowerTokens below one PowerGrid
+	// are fungible by monthly expiration date.
+	// TODO: Handle timezone and Daylight Saving Time.
+	mapping (uint64 => address) tokens;
 
-  function hasExpired(uint64 date) returns(bool) {
-    return block.timestamp > date;
-  }
+	function PowerGrid() {
+		owner = msg.sender;
+	}
 
-  modifier verifyExpirationDate(uint64 date) {
-    //TODO: convert uint timestamp to uint64
-    assert(!hasExpired(date));
-    _;
-  }
+	function create(
+		uint64 amount,
+		address destination,
+		uint64 expirationDate
+	) public
+	{
+		require(msg.sender == owner);
+		address tokenAddr = tokens[expirationDate];
+		PowerToken token;
+		if (tokenAddr == 0) {
+			token = new PowerToken(expirationDate);
+			tokens[expirationDate] = address(token);
+		} else {
+			token = PowerToken(tokenAddr);
+		}
+		token.create(amount, destination);
+	}
 
-  function create(uint64 amount, address destination, uint64 expirationDate) public
-    verifyExpirationDate(expirationDate)
-  {
-    require(msg.sender == owner);
-    address tokenAddr = tokens[expirationDate];
-    PowerToken token;
-    if (tokenAddr == 0) {
-      token = new PowerToken(expirationDate);
-      tokens[expirationDate] = address(token);
-    } else {
-      token = PowerToken(tokenAddr);
-    }
-    token.create(amount, destination);
-  }
-
-  function destroyToken(uint64 date) public {
-    if (hasExpired(date)) {
-      PowerToken(tokens[date]).destroy();
-      tokens[date] = 0;
-    }
-  }
+	function destroyToken(uint64 date) public {
+		PowerToken(tokens[date]).destroy();
+		tokens[date] = 0;
+	}
 }
--- powerToken.sol
+++ powerToken.sol
@@ -2,49 +2,97 @@ pragma solidity ^0.4.15;
 
 import './powerGrid.sol';
 
+contract DateTimeAPI {
+        /*
+         *  Abstract contract for interfacing with the DateTime contract.
+         *
+         */
+        function isLeapYear(uint16 year) constant returns (bool);
+        function getYear(uint timestamp) constant returns (uint16);
+        function getMonth(uint timestamp) constant returns (uint8);
+        function getDay(uint timestamp) constant returns (uint8);
+        function getHour(uint timestamp) constant returns (uint8);
+        function getMinute(uint timestamp) constant returns (uint8);
+        function getSecond(uint timestamp) constant returns (uint8);
+        function getWeekday(uint timestamp) constant returns (uint8);
+        function toTimestamp(uint16 year, uint8 month, uint8 day) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute, uint8 second) constant returns (uint timestamp);
+}
+
 contract PowerToken {
-  event Consumed(uint64 amount, uint code);
+	// Datetime library used in this contract:
+	address constant DATETIME_LIB = 0x1a6184CD4C5Bea62B0116de7962EE7315B7bcBce;
 
-  address owner;
-  uint64 expirationDate;
-  mapping(address => uint64) balances;
+	// Event registered when this token is consumed.
+	event Consumed(uint64 amount, uint code);
 
-  function PowerToken(uint64 date) {
-    owner = msg.sender;
-    expirationDate = date;
-  }
+	address owner;
 
-  modifier verifyExpirationDate(uint64 date) {
-    //TODO: convert uint timestamp to uint64
-    assert(block.timestamp < date);
-    _;
-  }
+	// Last Unix timestamp when this token is valid,
+	// stored so it won't have to be calculated twice.
+	uint expirationTime;
 
-  function create(uint64 amount, address destination) public {
-    require(msg.sender == owner);
-    balances[destination] += amount;
-  }
+	mapping(address => uint64) balances;
 
-  function transfer(address to, uint64 amount) public
-    verifyExpirationDate(expirationDate)
-  {
-    require(balances[msg.sender] >= amount);
-    require(balances[to] + amount > balances[to]);
-    balances[msg.sender] -= amount;
-    balances[to] += amount;
-  }
+	function monthToTimestamp(uint64 month) returns(uint)
+	{
+		// Add 1 so to get the first second of the next month.
+		month += 1;
 
-  function consume(uint64 amount, uint code) public
-    verifyExpirationDate(expirationDate)
-  {
-    require(balances[msg.sender] >= amount);
-    balances[msg.sender] -= amount;
-    Consumed(amount, code);
-  }
+		// Month 0 is January 1970:
+		uint16 y = 1970 + uint16(month / 12);
+		uint8 m = uint8(month % 12);
 
-  function destroy() {
-    require(msg.sender == owner);
-    address addr = PowerGrid(owner).owner();
-    selfdestruct(addr);
-  }
+		// Subtract one second, to get the last second of the valid month.
+		return DateTimeAPI(DATETIME_LIB).toTimestamp(y, m, 1) - 1;
+	}
+
+	function PowerToken(uint64 expirationMonth) {
+		expirationTime = monthToTimestamp(expirationMonth);
+
+		// TODO: Design choice: check expiration from here, too?
+
+		owner = msg.sender;
+	}
+
+	modifier checkExpired() {
+		require(block.timestamp <= expirationTime);
+		_;
+	}
+
+	function create(uint64 amount, address destination) public
+		checkExpired()
+	{
+		require(msg.sender == owner);
+		balances[destination] += amount;
+	}
+
+	function transfer(address to, uint64 amount) public
+		checkExpired()
+	{
+		require(balances[msg.sender] >= amount);
+		require(balances[to] + amount > balances[to]);
+		balances[msg.sender] -= amount;
+		balances[to] += amount;
+	}
+
+	function consume(uint64 amount, uint code) public
+		checkExpired()
+	{
+		require(balances[msg.sender] >= amount);
+		balances[msg.sender] -= amount;
+		Consumed(amount, code);
+	}
+
+	function destroy() {
+		require(block.timestamp > expirationTime);
+
+		// TODO: design choice: should anyone be able to destroy the contract?
+		require(msg.sender == owner);
+
+		address addr = PowerGrid(owner).owner();
+		selfdestruct(addr);
+	}
 }
